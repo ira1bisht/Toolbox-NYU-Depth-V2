7cc2e47655475204e86d9eea8c03433d507a818e
ira1bisht <ira1bisht@gmail.com>
2020-10-15 03:04:30 +0530

    Update demo_synched_projected_frames.m

--- demo_synched_projected_frames.m
+++ demo_synched_projected_frames.m
@@ -1,8 +1,8 @@
 % The directory where you extracted the raw dataset.
-datasetDir = '[PATH TO THE NYU DEPTH V2 RAW DATASET]';
+datasetDir = '/data/cafe';
 
 % The name of the scene to demo.
-sceneName = '[NAME OF A SCENE YOU WANT TO VIEW]';
+sceneName = 'cafe_0001a';
 
 % The absolute directory of the 
 sceneDir = sprintf('%s/%s', datasetDir, sceneName);
@@ -10,34 +10,21 @@ sceneDir = sprintf('%s/%s', datasetDir, sceneName);
 % Reads the list of frames.
 frameList = get_synched_frames(sceneDir);
 
+%create the video writer with 20 fps
+writerObj=VideoWriter('/data/cafe/scene1_video.avi');
+writerObj.FrameRate=20;
+open(writerObj);
+
 % Displays each pair of synchronized RGB and Depth frames.
-for ii = 1 : 15 : numel(frameList)
+for ii = 1 : numel(frameList)
   imgRgb = imread([sceneDir '/' frameList(ii).rawRgbFilename]);
   imgDepthRaw = swapbytes(imread([sceneDir '/' frameList(ii).rawDepthFilename]));
   
-  figure(1);
-  % Show the RGB image.
-  subplot(1,3,1);
-  imagesc(imgRgb);
-  axis off;
-  axis equal;
-  title('RGB');
-  
-  % Show the Raw Depth image.
-  subplot(1,3,2);
-  imagesc(imgDepthRaw);
-  axis off;
-  axis equal;
-  title('Raw Depth');
-  caxis([800 1100]);
-  
   % Show the projected depth image.
   imgDepthProj = project_depth_map(imgDepthRaw, imgRgb);
-  subplot(1,3,3);
-  imagesc(imgDepthProj);
-  axis off;
-  axis equal;
-  title('Projected Depth');
-  
-  pause(0.01);
+  frame=im2frame(imgDepthProj);
+  writeVideo(writerObj, frame);
 end
+
+%close the writer object
+close(writerObj)
